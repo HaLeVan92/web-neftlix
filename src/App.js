4544e37273d08daab00add2be6ec6bf0db2f2bc7
Van Ha <halevan0592@gmail.com>
2024-07-02 09:58:54 +0700

    api commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import List from './components/List';
 import Footer from './components/Footer';
+import apiService from './app/apiService';
+
+
 
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiService.post('/api/register', {
+          "email": "eve.holt@reqres.in",
+          "password": "pistol"
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();   
+  }, []);
+
+
   return (
+    
     <React.Fragment >
       <Header/>
       <Main/>
